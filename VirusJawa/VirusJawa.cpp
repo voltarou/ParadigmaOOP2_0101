264cefaa90c2ddc4081baca522e13f43670c65df
voltarou <bagusadnan04@gmail.com>
2024-05-27 11:01:01 +0800

    class lia

--- VirusJawa/VirusJawa.cpp
+++ VirusJawa/VirusJawa.cpp
@@ -9,7 +9,14 @@ public:
 class joko :public seseorang {
 public:
     void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
 
+class lia :public seseorang {
+public :
+    void pesan() {
+        cout << "Pesan dari lia" << endl;
     }
 };
 
