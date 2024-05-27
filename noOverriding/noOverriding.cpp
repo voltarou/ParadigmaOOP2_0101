b5c132ec8ad1f524d1a73229ed04e8dfe7f8f044
voltarou <bagusadnan04@gmail.com>
2024-05-27 11:25:45 +0800

    class baseclass public

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class baseClass {
+public :
+    virtual void perkenalan() {
+        cout << "Halo saya Function dari base Class";
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
 
