1077d942146671be7121af75a5a663380af65ce8
voltarou <bagusadnan04@gmail.com>
2024-05-27 11:11:55 +0800

    void setsaklar

--- Enkapsulasi/Enkapsulasi.cpp
+++ Enkapsulasi/Enkapsulasi.cpp
@@ -4,6 +4,11 @@ using namespace std;
 class remoteLampu {
 private :
     string saklarNo[10];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+
 };
 int main()
 {
